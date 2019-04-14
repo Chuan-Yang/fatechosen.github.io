a1f233e5a1ed4b3e715e6967624c7494ecfa7473
Chuan <idreamyc@gmail.com>
2019-04-14 15:27:42 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,18 @@
-$(document).ready(function(){
-$("#login").click(function(){
+
 var email = $("#email").val();
 var password = $("#password").val();
 // Checking for blank fields.
-if( email =='' || password ==''){
-$('input[type="text"],input[type="password"]').css("border","2px solid red");
-$('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
-alert("Please fill all fields...!!!!!!");
-}else {
-$.post("login.php",{ email1: email, password1:password},
-function(data) {
-if(data=='Invalid Email.......') {
-$('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-$('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-alert(data);
-}else if(data=='Email or Password is wrong...!!!!'){
-$('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-alert(data);
-} else if(data=='Successfully Logged in...'){
-$("form")[0].reset();
-$('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-alert(data);
-} else{
-alert(data);
-}
-});
-}
-});
+function success(){ console.log("Login OK"); alert("Login Succeeded"); }
+
+    function failure(){console.log("Login Failed");  alert("Login Failed"); }
+
+    function always(){ console.log("Login done"); alert("Login Request done"); }
+
+    function login(){
+      $.get("jQuery-and-JavaScript-login.php",
+       $("#loginForm").serialize()).done(success).fail(failure).always(always);
+      return false;
+    }
+$(document).ready(function(){
+    $("#login").click(login);
 });
