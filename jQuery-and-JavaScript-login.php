27a5271fd29e30a87259b85322a164dfbe004831
Chuan <idreamyc@gmail.com>
2019-04-14 22:14:50 -0600

    update

--- jQuery-and-JavaScript-login.php
+++ jQuery-and-JavaScript-login.php
@@ -1,14 +1,23 @@
 <?php
 header('Content-type: text/html');
-$logins = array('student' => 'student','tutor1' => 'tutor1','tutor2' => 'tutor2');
+$logins_s = array('student' => 'student');
+$logins_t = array('tutor1' => 'tutor1','tutor2' => 'tutor2');
 
 /* Check and assign submitted Username and Password to new variable */
 $Username = $_GET["user"];
 $Password = $_GET["pw"];
+$UserType = $_GET["userType"]
 /* Check Username and Password existence in defined array */
-if (isset($logins[$Username]) && $logins[$Username] == $Password){
-  echo "Welcome To Ajax!";
-  return;
+if ($UserType === "Student"){
+  if (isset($logins_s[$Username]) && $logins_s[$Username] == $Password){
+    echo "Welcome To Ajax!";
+    return;
+  }
+}else{
+  if (isset($logins_t[$Username]) && $logins_t[$Username] == $Password){
+    echo "Welcome To Ajax!";
+    return;
+  }
 }
 header("HTTP/1.0 400 Login Failed");
 echo "Invalid Credentials.";
