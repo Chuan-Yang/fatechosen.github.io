5372ce755fdbedfcef58feaf96e4df9459504068
Chuan <idreamyc@gmail.com>
2019-04-14 22:40:53 -0600

    update

--- jQuery-and-JavaScript-login.php
+++ jQuery-and-JavaScript-login.php
@@ -1,12 +1,17 @@
 <?php
 header('Content-type: text/html');
-$logins = array('student' => 'student','tutor1' => 'tutor1','tutor2' => 'tutor2');
-
+$logins_s = array('st' => 'st');
+$logins_t = array('t1' => 't1','t2' => 't2');
 /* Check and assign submitted Username and Password to new variable */
 $Username = $_GET["user"];
 $Password = $_GET["pw"];
+$type = $_GET["userType"];
 /* Check Username and Password existence in defined array */
-if (isset($logins[$Username]) && $logins[$Username] == $Password){
+if (isset($logins_s[$Username]) && $logins_s[$Username] == $Password && $type == "Student"){
+  echo "Welcome To Ajax!";
+  return;
+}
+if (isset($logins_t[$Username]) && $logins_t[$Username] == $Password && $type == "Tutor"){
   echo "Welcome To Ajax!";
   return;
 }
